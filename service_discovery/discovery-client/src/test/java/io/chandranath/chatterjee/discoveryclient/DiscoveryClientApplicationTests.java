a57a3a841b08e3acd8abab04dcc6711f3e8c6f40
Chandranath Chatterjee <chandranathchatterjee@Chandranaths-MacBook-Pro.local>
2020-04-19 19:26:42 +0530

    draft push

---
+++ service_discovery/discovery-client/src/test/java/io/chandranath/chatterjee/discoveryclient/DiscoveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chandranath.chatterjee.discoveryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
